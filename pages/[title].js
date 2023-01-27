5fa9cec316d5a12442f27932f960663cc44d3a37
NicoooM <nmarsan33000@gmail.com>
2023-01-27 13:31:03 +0100

    add files

--- pages/[title].js
+++ pages/[title].js
@@ -1,19 +1,28 @@
+import data from "../src/db.json";
+
+export default function Post({ company }) {
+  return <div>{company.title}</div>;
+}
+
 // Generates `/posts/1` and `/posts/2`
 export async function getStaticPaths() {
+  const paths = data.map((company) => ({
+    params: { title: company.title },
+  }));
+
   return {
-    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    paths: paths,
     fallback: false, // can also be true or 'blocking'
   };
 }
 
 // `getStaticPaths` requires using `getStaticProps`
 export async function getStaticProps(context) {
+  const { params } = context;
+  const { title } = params;
+  const company = data.find((company) => company.title === title);
   return {
     // Passed to the page component as props
-    props: { post: {} },
+    props: { company },
   };
-}
-
-export default function Post({ post }) {
-  // Render post...
 }
